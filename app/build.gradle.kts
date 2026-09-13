afb937350b62131c7fe543dff1496859bbca20b2
frRitamDas <ritamdas20012@gmail.com>
2026-09-14 00:11:36 +0530

    chore: mark 1.6 beta version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.music.pexpo"
         minSdk = 26
         targetSdk = 36
-        versionCode = 21
-        versionName = "1.5.7"
+        versionCode = 22
+        versionName = "1.6.0-beta.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "LASTFM_API_KEY", "\"${lastfmApiKey.replace("\\", "\\\\").replace("\"", "\\\"")}\"")
         buildConfigField("String", "LASTFM_SECRET", "\"${lastfmSecret.replace("\\", "\\\\").replace("\"", "\\\"")}\"")
